ed997c1d125720103bd23001dfd1fbe266aeb76c
Danny <danny@mail.com>
2020-03-02 13:10:10 +0100

    3

---
+++ 03-wk2018.js
@@ -0,0 +1,7 @@
+function toonWinnaars(){
+	var winnaars=["Frankrijk","Kroatië","België"];
+	var lengte= winnaars.length;
+	for (var i=0;  i <lengte; i++) {
+		document.write("<p>Op plaats " + (i+1) + " staat : " + winnaars[i] + ".</p>")
+	}
+}
